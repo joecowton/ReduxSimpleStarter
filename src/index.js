fe9c1da77b11f734dd752492e1def4f260a156ce
Joe Cowton <joecowton@gmail.com>
2018-01-24 11:00:34 +0000

    adds component files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-// Create anew compmeonent, this should produce HTML.
+
+
 const App = () => {
   return <div>Hi!</div>;
 }
-
-
 
 // Take HTML and render to DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
